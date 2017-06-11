2d628937a03e4d0cefaadec5cf8f8769805eb287
Pieter Gorsira <pgorsira@gmail.com>
2017-06-10 23:30:06 -0400

    Split contracts into separate files as required by Truffle

---
+++ contracts/Curator.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.0;
+
+import "./CurationToken.sol";
+
+/*
+Back information with full backing in that subtopic
+Currently just uses event logs.
+Have to build a local DB to filter these events.
+No internal storage atm.
+*/
+contract Curator {
+
+    function back(address _token, string _subtopic, string _info) {
+        CurationToken token = CurationToken(_token);
+        LogBacking(msg.sender, _info, token.totalBondsPerCuratorPerSubtopic(msg.sender, _subtopic), token.totalBonded());
+    }
+
+    function revoke(address _token, string _subtopic, string _info) {
+        CurationToken token  = CurationToken(_token);
+        LogRevoke(msg.sender, _info, token.totalBondsPerCuratorPerSubtopic(msg.sender, _subtopic), token.totalBonded());
+    }
+
+    event LogBacking(address curator, string info, uint256 bondedAmount, uint256 totalBonded);
+    event LogRevoke(address curator, string info, uint256 bondedAmount, uint256 totalBonded);
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,12 @@
 var ERC20Token = artifacts.require("./ERC20Token.sol");
-var CurationMarkets = artifacts.require("./CurationMarkets.sol");
+var ContinuousToken = artifacts.require("./ContinuousToken.sol");
+var CurationToken = artifacts.require("./CurationToken.sol");
+var Curator = artifacts.require("./Curator.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ERC20Token);
-  deployer.link(ERC20Token, CurationMarkets);
-  deployer.deploy(CurationMarkets);
+  deployer.deploy(ContinuousToken);
+  deployer.deploy(CurationToken);
+  deployer.deploy(Curator);
+  deployer.autolink();
 };
